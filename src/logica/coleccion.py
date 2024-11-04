d2bcad3c4ad2acabd8bb1b885efb787e3c390e84
Alejandra Uniandes <ma.ninog12@uniandes.edu.co>
2024-11-03 20:01:32 -0500

    ajuste code smell 3: Ajuste a clase

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -294,3 +294,28 @@ class Coleccion:
             .all()
         ]
         return interpretes
+
+    def buscar_interprete(self, buscar, interprete_nombre):
+        buscar = True
+        if buscar:
+            try:
+                interpretes = [
+                    elem.__dict__
+                    for elem in session.query(Interprete)
+                    .filter(Interprete.nombre.ilike("%{0}%".format(interprete_nombre)))
+                    .all()
+                ]
+            except Exception:
+                pass
+        return interpretes
+
+    def dar_interpretes_2(self):
+        dar = True
+        if dar:
+            try:
+                interpretes = [
+                    elem.__dict__ for elem in session.query(Interprete).all()
+                ]
+            except Exception:
+                pass
+        return interpretes
